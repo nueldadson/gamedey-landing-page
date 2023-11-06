48669f01018ce71d01eae4087a052af42e392225
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2023-11-06 13:22:52 +0100

    Update Hero.js

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,17 +1,23 @@
-import react from 'react';
+import react, { useEffect } from 'react';
 import { Slide } from 'react-awesome-reveal';
 import Button from "../components/Button";
 import {heroIMG} from '../assets/images';
 import {heroIMG2} from '../assets/images';
 import {heroIMG3} from '../assets/images';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // import { useEffect } from 'react';
 import './hero.css';
 
 
 function Hero () {
 
+    useEffect(()=> {
+        AOS.init({duration: 2000});
+    }, []);
+
     return (
-        <div id="home" className="relative flex flex-col font-bold justify-center items-center w-full  min-h-screen  px-4 lg:px-24 md:px-24  py-24 lg:py-24 md:py-24 ">
+        <div id="home" className="relative flex flex-col font-bold justify-center items-center w-full  min-h-screen  px-4 lg:px-24 md:px-24  py-24 lg:py-24 md:py-24 " data-aos="fade-up">
                         <img src={heroIMG} alt="Hero Image" class="  lg:w-2/6 md:w-2/3 w-5/6  mb-2 pt-8" />
                         <h1 className="text-2xl md:text-3xl lg:text-4x text-center px-4 md:px-24 lg:px-60 py-4 md:py-2 lg:py-2 mb-4 md:mb-6 lg:mb-8">
                             Gamedey is a social platform for fitness enthusiasts of all backgrounds, where you can connect, have fun, and unleash your competitive spirit! Playing games with your friends doesn't have to be a hassle anymore. No matter your hobby, skill level, or preferred time, Gamedey gives you the ultimate experience.
