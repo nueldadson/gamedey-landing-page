85b32bea61cabe47cdbd17a3b841665d68b0a11f
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2023-11-06 07:34:58 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -38,7 +38,7 @@ const Nav = ({ isOpen, toggle }) => {
 					<img
 						src={Logo}
 						alt="logo"
-						className="mt-1 w-[30px] pl-20"
+						className="mt-1 w-[30px] pr-20"
 					/>
 				</a>
 				<h1 className="logotext font-bold max-lg:hidden">
