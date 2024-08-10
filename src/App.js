b95a78ef8836677374737416562fd90e2dc02848
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2024-08-10 23:02:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -110,6 +110,32 @@ const timer3 = setTimeout(() => {
 				<AboutUs />
 				<GetStarted />
 				<SafeHaven />
+	    <div id="mc_embed_shell">
+  
+<div id="mc_embed_signup">
+    <form action="https://gamedey.us14.list-manage.com/subscribe/post?u=cd0e8d687a0483e5ef74ca4c5&amp;id=cc2b3a4692&amp;f_id=0033b8e5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
+        <div id="mc_embed_signup_scroll">
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div><div class="mc-field-group"><label for="mce-FNAME">Name <span class="asterisk">*</span></label><input type="text" name="FNAME" class="required text" id="mce-FNAME" required="" value=""></div>
+        <div id="mce-responses" class="clear foot">
+            <div class="response" id="mce-error-response" style="display: none;"></div>
+            <div class="response" id="mce-success-response" style="display: none;"></div>
+        </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+        <input type="text" name="b_cd0e8d687a0483e5ef74ca4c5_cc2b3a4692" tabindex="-1" value="">
+    </div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
+                <p style="margin: 0px auto;"><a href="http://eepurl.com/iWKdTY" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"></span></a></p>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+</div>
+
 				<FAQsection />
 				<Footer />
 			</div>
