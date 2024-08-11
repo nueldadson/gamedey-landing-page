aa500a8d793fa0d24dacb0dcc599fc6f1786d48f
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2024-08-11 13:03:08 +0100

    Update waitlist.js

--- src/components/waitlist.js
+++ src/components/waitlist.js
@@ -54,6 +54,97 @@ const Waitlist = ({ toggleActive }) => {
             <input type="submit" value="Send" />
         
         </form>
+		    <form
+                    action="https://gamedey.us14.list-manage.com/subscribe/post?u=cd0e8d687a0483e5ef74ca4c5&amp;id=cc2b3a4692&amp;f_id=0033b8e5f0"
+                    method="post"
+                    id="mc-embedded-subscribe-form"
+                    name="mc-embedded-subscribe-form"
+                    className="validate"
+                    target="_blank"
+                >
+                    <div id="mc_embed_signup_scroll">
+                        <div className="indicates-required">
+                            <span className="asterisk">*</span> indicates required
+                        </div>
+                        <div className="mc-field-group">
+                            <label htmlFor="mce-EMAIL">
+                                Email Address <span className="asterisk">*</span>
+                            </label>
+                            <input
+                                type="email"
+                                name="EMAIL"
+                                className="required email"
+                                id="mce-EMAIL"
+                                required
+                                defaultValue=""
+                            />
+                        </div>
+                        <div className="mc-field-group">
+                            <label htmlFor="mce-FNAME">
+                                Name <span className="asterisk">*</span>
+                            </label>
+                            <input
+                                type="text"
+                                name="FNAME"
+                                className="required text"
+                                id="mce-FNAME"
+                                required
+                                defaultValue=""
+                            />
+                        </div>
+                        <div id="mce-responses" className="clear foot">
+                            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+                            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+                        </div>
+                        <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px' }}>
+                            <input
+                                type="text"
+                                name="b_cd0e8d687a0483e5ef74ca4c5_cc2b3a4692"
+                                tabIndex="-1"
+                                defaultValue=""
+                            />
+                        </div>
+                        <div className="optionalParent">
+                            <div className="clear foot">
+                                <input
+                                    type="submit"
+                                    name="subscribe"
+                                    id="mc-embedded-subscribe"
+                                    className="button"
+                                    value="Subscribe"
+                                />
+                                <p style={{ margin: '0px auto' }}>
+                                    <a
+                                        href="http://eepurl.com/iWKdTY"
+                                        title="Mailchimp - email marketing made easy and fun"
+                                    >
+                                        <span
+                                            style={{
+                                                display: 'inline-block',
+                                                backgroundColor: 'transparent',
+                                                borderRadius: '4px',
+                                            }}
+                                        >
+                                            <img
+                                                className="refferal_badge"
+                                                src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
+                                                alt="Intuit Mailchimp"
+                                                style={{
+                                                    width: '220px',
+                                                    height: '40px',
+                                                    display: 'flex',
+                                                    padding: '2px 0px',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                }}
+                                            />
+                                        </span>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </form>
     </div>
 </div>
     )}
